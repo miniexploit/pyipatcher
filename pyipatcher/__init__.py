c997229cc803b3d1917df9dc7d7313bbf5e8e412
m1sta <adamhamdi31@gmail.com>
2023-01-17 14:54:34 -0600

    Update __init__

--- pyipatcher/__init__.py
+++ pyipatcher/__init__.py
@@ -1,5 +1,5 @@
 from importlib.metadata import version
 
-from .patchfinder64 import patchfinder64
+from .patchfinder64 import patchfinder64, arm64_branch_instruction
 
 __version__ = version(__package__)
